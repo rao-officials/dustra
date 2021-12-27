a4f9a50a281ab48b9aad6446a091259c65abeac8
Haseeb <haseeb@Haseebs-iMac.local>
2021-12-27 15:46:14 +0500

    add angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { TeamMembersComponent } from './components/team-members/team-members.component';
+import { WorkProcessComponent } from './components/work-process/work-process.component';
+import { OurServicesComponent } from './components/our-services/our-services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TestimonialsComponent,
+    TeamMembersComponent,
+    WorkProcessComponent,
+    OurServicesComponent
   ],
   imports: [
     BrowserModule,
